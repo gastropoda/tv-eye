ff12dfa0b181cdf5edf9f332dff4dd6386c25ca4
Artem Baguinski <abaguinski@depraktijkindex.nl>
2014-01-14 15:40:40 +0100

    peek at pixels

--- src/js/scratch.js
+++ src/js/scratch.js
@@ -3,19 +3,31 @@ $(function() {
     return $("<canvas/>").attr("id", id).attr("width", width).attr("height", height);
   };
 
+  function getCanvas( element ) {
+    return element.get(0).getContext("2d");
+  };
+
   function replaceImageWithCanvas(image, canvasId) {
     var imageWidth = image.width();
     var imageHeight = image.height();
 
-    var canvasElement = makeCanvas("input", image.width(), image.height());
-    var canvas = canvasElement.get(0).getContext("2d");
+    var canvasElement = makeCanvas(canvasId, image.width(), image.height());
+    var canvas = getCanvas(canvasElement);
     canvas.drawImage(image.get(0), 0, 0);
     image.replaceWith( canvasElement );
-    return canvas;
-  }
+    return canvasElement;
+  };
 
   $(window).load( function() {
-    var canvas = replaceImageWithCanvas( $("img#input_image") );
+    var canvasElement = replaceImageWithCanvas( $("img#input_image"), "input" );
+    var canvas = getCanvas(canvasElement);
+
+    canvasElement.click(function(e) {
+      var x = e.offsetX, y = e.offsetY;
+      var rgba = canvas.getImageData(x,y,1,1).data;
+      console.log("x: " + x + ", y: " + y +
+        "r: " + rgba[0] + ", g: " + rgba[1] + ", b: " + rgba[2]);
+    });
   });
 });
 
