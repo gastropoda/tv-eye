4e2f8ef739db86cdb78c5685df473af9d7e29bf9
Artem Baguinski <abaguinski@depraktijkindex.nl>
2014-01-15 15:09:07 +0100

    set and test canvas size

--- spec/flood-fill_spec.js
+++ spec/flood-fill_spec.js
@@ -3,13 +3,20 @@ define(["flood-fill", "jquery"], function(floodFill, $) {
   describe("testing canvas", function() {
 
     before(function() {
-      $("<canvas/>")
+      this.canvas = $("<canvas/>")
         .attr("id", "scratch")
+        .width(8).height(8)
         .appendTo("#mocha");
     });
 
     it("gets created", function() {
       expect($("#mocha canvas#scratch").length).to.equal(1);
+      expect(this.canvas.length).to.equal(1);
+    });
+
+    it("is sufficiently small", function() {
+      expect(this.canvas.width()).to.equal(8);
+      expect(this.canvas.height()).to.equal(8);
     });
   });
 
