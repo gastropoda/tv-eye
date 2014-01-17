31ea4984a2771bb20d67c0a4a5e5874e876939d7
Artem Baguinski <abaguinski@depraktijkindex.nl>
2014-01-17 01:24:43 +0100

    slight refactoring in scratch

--- src/js/scratch.js
+++ src/js/scratch.js
@@ -6,35 +6,36 @@ define([
 ], function($, floodFill) {
 
   function configure() {
-    $("#scratch").loadImageCanvas("img/groningen-test.jpg", function(canvas) {
 
+    var imageData, context;
+
+    function clickHandler(e) {
+      var x = e.offsetX;
+      var y = e.offsetY;
+      var c = imageData.color(x, y);
+
+      var patch = imageData.floodFill(x, y, 30, 150);
+      console.log("x: " + x + ", y: " + y + "r: " + c.r + ", g: " + c.g + ", b: " + c.b);
+      console.log("patch area: " + patch.area + ", avg color r: " + patch.averageColor.r +
+        ", g: " + patch.averageColor.g + ", b: " + patch.averageColor.b);
+
+      var dirtyCorner = patch.bounds[0];
+      var dirtySize = [patch.bounds[1][0] - dirtyCorner[0], patch.bounds[1][1] - dirtyCorner[1]];
+      context.putImageData(
+        imageData,
+        0, 0,
+        dirtyCorner[0], dirtyCorner[1],
+        dirtySize[0], dirtySize[1]);
+    };
+
+    $("#scratch").loadImageCanvas("img/groningen-test.jpg", function(canvas) {
       var canvasWidth = canvas.width();
       var canvasHeight = canvas.height();
 
-      var context = canvas.context2d();
-      var imageData = context.getImageData(0, 0, canvasWidth, canvasHeight);
-
+      context = canvas.context2d();
+      imageData = context.getImageData(0, 0, canvasWidth, canvasHeight);
       floodFill.extend(imageData);
-
-      canvas.on("click", function(e) {
-        var x = e.offsetX;
-        var y = e.offsetY;
-        var c = imageData.color(x, y);
-
-        var patch = imageData.floodFill(x, y, 30, 150);
-        console.log("x: " + x + ", y: " + y + "r: " + c.r + ", g: " + c.g + ", b: " + c.b);
-        console.log("patch area: " + patch.area + ", avg color r: " + patch.averageColor.r +
-          ", g: " + patch.averageColor.g + ", b: " + patch.averageColor.b);
-
-        var dirtyCorner = patch.bounds[0];
-        var dirtySize = [patch.bounds[1][0] - dirtyCorner[0], patch.bounds[1][1] - dirtyCorner[1]];
-        context.putImageData(
-          imageData,
-          0, 0,
-          dirtyCorner[0], dirtyCorner[1],
-          dirtySize[0], dirtySize[1]);
-      });
-
+      canvas.on("click", clickHandler);
       canvas.hide().fadeIn(300);
     });
   };
