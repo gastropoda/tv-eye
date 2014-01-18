ced5b43067f7c23195f8e30d6bfedc85470e45f9
Artem Baguinski <abaguinski@depraktijkindex.nl>
2014-01-18 01:00:21 +0100

    delete patch unpaints it as well

--- src/js/app-view-model.js
+++ src/js/app-view-model.js
@@ -25,12 +25,6 @@ define([
 
     self.findPatch = function(x, y) {
       var patch = imageData.floodFill(x, y, 30, nextPatchIndex());
-      context.putImageData(
-        imageData,
-        0, 0,
-        patch.bounds.x, patch.bounds.y,
-        patch.bounds.w, patch.bounds.h);
-
       return new ColorPatch({
         area: patch.area,
         color: htmlizeColor(patch.averageColor),
@@ -39,8 +33,18 @@ define([
     };
 
     self.removePatch = function(patch) {
-      patchList.remove(patch)
+      var index = patchList.remove(patch);
+      imageData.replaceIndex(index, NO_PATCH, patch.bounds());
+      refreshPatch(patch);
     };
+
+    function refreshPatch(patch) {
+      context.putImageData(
+        imageData,
+        0, 0,
+        patch.bounds().x, patch.bounds().y,
+        patch.bounds().w, patch.bounds().h);
+    }
 
     self.onCanvasClick = function(e) {
       var x = e.offsetX;
@@ -48,11 +52,14 @@ define([
       var c = imageData.color(x, y);
 
       if (c.a == NO_PATCH) {
-        patchList.put(self.findPatch(x, y));
+        var patch = self.findPatch(x, y);
+        patchList.put(patch);
+        refreshPatch(patch);
       }
       else {
         patchList.get(c.a).toggleSelected();
       }
+
     };
 
     $("#scratch").loadImageCanvas("img/groningen-test.jpg", function(canvas) {
