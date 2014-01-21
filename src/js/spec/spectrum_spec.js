2281b8bc7d61fa626cd86dc508edf533e51f45ca
Artem Baguinski <abaguinski@depraktijkindex.nl>
2014-01-21 21:20:13 +0100

    Spectrum's recognizeShade()

--- src/js/spec/spectrum_spec.js
+++ src/js/spec/spectrum_spec.js
@@ -2,15 +2,57 @@ define([
   "spectrum"
 ], function(Spectrum) {
   describe("Spectrum", function() {
-    var pink = {};
-    var green = {};
-    var yellow = {};
-    var shades = [pink, green, yellow];
-    var spectrum = new Spectrum({shades: shades});
+    var pink;
+    var green;
+    var yellow;
+    var inputColor;
+    var shades;
+    var shadeTolerance;
+    var spectrum;
+
+    beforeEach(function() {
+      pink = {};
+      green = {};
+      yellow = {};
+      inputColor = {};
+      shades = [pink, green, yellow];
+      shadeTolerance = 50;
+      spectrum = new Spectrum({shades: shades, shadeTolerance: shadeTolerance});
+    });
 
     describe("constructor", function() {
       it("assigns shades", function() {
         expect(spectrum.shades).to.eql(shades);
+      });
+
+      it("assigns shade tolerance", function() {
+        expect(spectrum.shadeTolerance).to.eql(shadeTolerance);
+      });
+    });
+
+    describe("recognize shade", function() {
+      function testRecognizer( distances, expectedResult) {
+        $.each(shades,function(i, shade) {
+          shade.distance = sinon.stub().returns( distances[i] );
+        });
+        expect(spectrum.recognizeShade(inputColor)).to.eq(expectedResult);
+        $.each(shades,function(i, shade) {
+          expect(shade.distance).to.have.been.calledWith(inputColor);
+        });
+      }
+
+      it("returns null if input is too far from any shade", function() {
+        testRecognizer([51,151,251], null);
+      });
+      it("returns the shade within shade tolerance", function() {
+        testRecognizer([50,151,251], pink);
+        testRecognizer([150,30,251], green);
+        testRecognizer([150,151,25], yellow);
+      });
+      it("returns the closes shade if multiple candidates", function() {
+        testRecognizer([5,30,151], pink);
+        testRecognizer([50,30,51], green);
+        testRecognizer([50,30,5], yellow);
       });
     });
   });
--- src/js/spectrum.js
+++ src/js/spectrum.js
@@ -1,9 +1,30 @@
 define([
-], function() {
+  "jquery"
+], function($) {
   function Spectrum(options) {
     options = options || {};
     this.shades = options.shades;
+    this.shadeTolerance = options.shadeTolerance;
   }
+
+  $.extend(Spectrum.prototype, {
+
+    recognizeShade: function( color ) {
+      var minDistance = Infinity;
+      var closestShade = null;
+      var self = this;
+
+      $.each(this.shades, function(i, shade) {
+        var distance = shade.distance(color);
+        if (distance <= Math.min(self.shadeTolerance, minDistance)) {
+          minDistance = distance;
+          closestShade = shade;
+        }
+      });
+
+      return closestShade;
+    }
+  });
 
   return Spectrum;
 });
