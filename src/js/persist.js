58194057c31891145bb7995e98c0f2074cc73f0f
Artem Baguinski <abaguinski@depraktijkindex.nl>
2014-01-30 08:32:00 +0100

    custom deserialization support in persist

--- src/js/persist.js
+++ src/js/persist.js
@@ -3,12 +3,15 @@ define([
 ], function(ko) {
   return persist;
 
-  function persist(key, observable) {
+  function persist(key, observable, fromJSON) {
     observable.subscribe(function(newValue) {
       localStorage[key] = ko.toJSON(newValue);
     });
     try {
       var storedValue = JSON.parse(localStorage[key]);
+      if (fromJSON) {
+        storedValue = fromJSON(storedValue);
+      }
       observable(storedValue);
     } catch(e) {
       observable.valueHasMutated();
