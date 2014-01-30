60c258f60846ce488b4c7d8911edba6ca7c0bb68
Artem Baguinski <abaguinski@depraktijkindex.nl>
2014-01-30 08:31:09 +0100

    depend on lazy

--- src/js/common-require-settings.js
+++ src/js/common-require-settings.js
@@ -3,7 +3,8 @@ require.config({
     jquery: "../../bower_components/jquery/jquery",
     paper: "../../bower_components/paper/dist/paper",
     knockout: "../../bower_components/knockout.js/knockout.debug",
-    bootstrap: "../../bower_components/bootstrap/dist/js/bootstrap.min"
+    bootstrap: "../../bower_components/bootstrap/dist/js/bootstrap.min",
+    lazy: "../../bower_components/lazy.js/lazy"
   },
 
   map: {
@@ -18,6 +19,9 @@ require.config({
   shim: {
     bootstrap: {
       deps: ["jquery"]
+    },
+    lazy: {
+      exports: "Lazy"
     }
   }
 });
